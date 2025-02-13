8550b010c7d956fd3a9ded9cde49d355882ff1de
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2025-02-13 21:13:19 +0100

    Declared three plugins. However, I'm getting unexpected results, as documented in README.md

---
+++ Plugin/PA10ba.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Denal05\Ad0e702ExerciseSeePluginExecOrderViaLog\Plugin;
+
+use Exception;
+use Psr\Log\LoggerInterface as PsrLoggerInterface;
+
+class PA10ba
+{
+    private $logger;
+
+    public function __construct(
+        PsrLoggerInterface $logger
+    )
+    {
+        $this->logger = $logger;
+    }
+
+    public function beforeExecute(
+        \Magento\Cms\Controller\Index\Index $subject
+    ) {
+        try {
+            $this->logger->debug(__METHOD__);
+        } catch (Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
+        return null;
+    }
+
+    public function afterExecute(
+        \Magento\Cms\Controller\Index\Index $subject,
+        $result
+    ) {
+        try {
+            $this->logger->debug(__METHOD__);
+        } catch (Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
+        return $result;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Ad0e702ExerciseSeePluginExecOrderViaLog
 An AD0-E702 certification exercise module: see the execution order of plugins via the debug log.
+
+I'm getting strange results. Expected:
+```php
+Plugin A (sort order 10): beforeExecute()
+Plugin B (sort order 20): beforeExecute()
+Plugin C (sort order 30): beforeExecute()
+Plugin A (sort order 10): afterExecute()
+Plugin B (sort order 20): afterExecute()
+Plugin C (sort order 30): afterExecute()
+```
+but instead I got the following results:
+```php
+
+Plugin C (sort order 30): beforeExecute()
+Plugin C (sort order 30): afterExecute()
+```
+
+The following is a relevant excerpt from `var/log/debug.log`
+```php
+[2025-02-13T20:05:34.036029+00:00] main.DEBUG: Denal05\Ad0e702ExerciseSeePluginExecOrderViaLog\Plugin\PC30ba::beforeExecute [] []
+[2025-02-13T20:05:34.589609+00:00] main.DEBUG: Denal05\Ad0e702ExerciseSeePluginExecOrderViaLog\Plugin\PC30ba::afterExecute [] []
+```
